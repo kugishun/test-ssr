4e37401acf4224a9e3ef8d5d2bcaca7721008624
kugimiya shun <s1290195@u-aizu.ac.jp>
2024-03-26 19:13:39 +0900

    Route pathの変更

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 
 export const Router: React.FC = () => {
+  const URL = process.env.PUBLIC_URL;
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+      <Routes /*location={location} key={location.pathname}*/>
+        <Route path={URL} element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
